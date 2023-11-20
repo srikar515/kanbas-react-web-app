9c0efd4c1d7ef787700441608faf6219c1505d81
Nallapu Srikar <nallapusrikar@Nallapus-MacBook-Air.local>
2023-11-20 17:29:37 -0500

    Updated code

--- src/Labs/a5/index.js
+++ src/Labs/a5/index.js
@@ -5,9 +5,6 @@ function Assignment5(){
     return (
         <div>
             <h1>Assignment 5</h1>
-            <div className="list-group">
-                <a href="http://localhost:4000/a5/welcome" className="list-group-item"> Welcome</a>  
-            </div>
             <EncodingParametersInURLs/>
             <WorkingWithObjects/>
             <WorkingWithArrays/>
