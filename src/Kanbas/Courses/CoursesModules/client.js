dcef8f65ce2cbeb4edd4bf524ac0290f31be6426
Nallapu Srikar <nallapusrikar@Nallapus-MacBook-Air.local>
2023-11-20 22:08:41 -0500

    Added files

--- src/Kanbas/Courses/CoursesModules/client.js
+++ src/Kanbas/Courses/CoursesModules/client.js
@@ -1,6 +1,9 @@
 import axios from 'axios';
-const COURSES_URL = 'http://localhost:4000/api/courses';
-const MODULES_URL = 'http://localhost:4000/api/modules';
+const API_BASE = process.env.REACT_APP_API_BASE;
+const MODULES_URL = `${API_BASE}/modules`;
+const COURSES_URL = `${API_BASE}/courses`;
+//const COURSES_URL = 'https://kanbas-node-server-app-3yx0.onrender.com/api/courses';
+//const MODULES_URL = 'https://kanbas-node-server-app-3yx0.onrender.com/api/modules';
 
 export const removeModule = async (moduleId) => {
   const response = await axios.delete(`${MODULES_URL}/${moduleId}`);
